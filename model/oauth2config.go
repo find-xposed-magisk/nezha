731bc9521ad96718efc770a327b5534a40ec1132
UUBulb <35923940+uubulb@users.noreply.github.com>
2025-03-04 20:28:22 +0800

    fix: oauth2 config not loaded (#1018)

--- model/oauth2config.go
+++ model/oauth2config.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Oauth2Config struct {
-	ClientID     string         `mapstructure:"client_id" json:"client_id,omitempty"`
-	ClientSecret string         `mapstructure:"client_secret" json:"client_secret,omitempty"`
-	Endpoint     Oauth2Endpoint `mapstructure:"endpoint" json:"endpoint,omitempty"`
-	Scopes       []string       `mapstructure:"scopes" json:"scopes,omitempty"`
+	ClientID     string         `koanf:"client_id" json:"client_id,omitempty"`
+	ClientSecret string         `koanf:"client_secret" json:"client_secret,omitempty"`
+	Endpoint     Oauth2Endpoint `koanf:"endpoint" json:"endpoint,omitempty"`
+	Scopes       []string       `koanf:"scopes" json:"scopes,omitempty"`
 
-	UserInfoURL string `mapstructure:"user_info_url" json:"user_info_url,omitempty"`
-	UserIDPath  string `mapstructure:"user_id_path" json:"user_id_path,omitempty"`
+	UserInfoURL string `koanf:"user_info_url" json:"user_info_url,omitempty"`
+	UserIDPath  string `koanf:"user_id_path" json:"user_id_path,omitempty"`
 }
 
 type Oauth2Endpoint struct {
-	AuthURL  string `mapstructure:"auth_url" json:"auth_url,omitempty"`
-	TokenURL string `mapstructure:"token_url" json:"token_url,omitempty"`
+	AuthURL  string `koanf:"auth_url" json:"auth_url,omitempty"`
+	TokenURL string `koanf:"token_url" json:"token_url,omitempty"`
 }
 
 func (c *Oauth2Config) Setup(redirectURL string) *oauth2.Config {
